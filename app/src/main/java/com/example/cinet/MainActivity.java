0f07a2239668a56fb5c21998a40be89f684dbe56
Pol <pbermejo@elpuig.xeill.net>
2022-04-19 18:43:01 +0200

    draw menu

--- app/src/main/java/com/example/cinet/MainActivity.java
+++ app/src/main/java/com/example/cinet/MainActivity.java
@@ -2,32 +2,56 @@ package com.example.cinet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.cinet.databinding.ActivityMainBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+    Fragment fragment=null;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView((binding = binding.inflate(getLayoutInflater())).getRoot());
 
-        //signInClient.launch(GoogleSignIn.getClient(this, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).build()).getSignInIntent());
+
+        setSupportActionBar(binding.toolbar);
+        // getSupportActionBar().hide();
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+
+        // draw menu
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                // Top-level destinations:
+                R.id.homeFragment, R.id.perfilFragment
+        )
+                .setOpenableLayout(binding.drawerLayout)
+                .build();
+
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
+
     }
-/*
-    ActivityResultLauncher<Intent> signInClient = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-        try {
-            FirebaseAuth.getInstance().signInWithCredential(GoogleAuthProvider.getCredential(GoogleSignIn.getSignedInAccountFromIntent(result.getData()).getResult(ApiException.class).getIdToken(), null));
-        } catch (ApiException e) {}
-    });
- */
 
+    /*public void homeact(View view) {
+        startActivity(new Intent(this, HomeActivity.class));
+    }*/
 }
