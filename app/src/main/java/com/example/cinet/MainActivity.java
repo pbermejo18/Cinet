25e4cf9e7b9251501965ac9442b72575dc4486a3
Pol <pbermejo@elpuig.xeill.net>
2022-04-19 20:40:06 +0200

    colors

--- app/src/main/java/com/example/cinet/MainActivity.java
+++ app/src/main/java/com/example/cinet/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cinet;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -36,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView((binding = binding.inflate(getLayoutInflater())).getRoot());
-
 
         setSupportActionBar(binding.toolbar);
         // getSupportActionBar().hide();
