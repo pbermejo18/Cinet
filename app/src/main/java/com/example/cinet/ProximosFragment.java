1556e6ad42b4b72bc45c0595f08757f2e1d08470
Pol <pbermejo@elpuig.xeill.net>
2022-04-28 17:53:30 +0200

    recycler view de 2 en 2

--- app/src/main/java/com/example/cinet/ProximosFragment.java
+++ app/src/main/java/com/example/cinet/ProximosFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -23,6 +24,7 @@ import com.google.firebase.firestore.Query;
 public class ProximosFragment extends Fragment {
 
     NavController navController;
+    RecyclerView.LayoutManager mLayoutManager;
 
     public ProximosFragment() {}
 
@@ -39,6 +41,9 @@ public class ProximosFragment extends Fragment {
         // navController = Navigation.findNavController(view);  // <-----------------
 
         RecyclerView postsRecyclerView = view.findViewById(R.id.peliculasRecyclerView);
+        // posición de las películas
+        mLayoutManager = new GridLayoutManager(getContext(), 2);
+        postsRecyclerView.setLayoutManager(mLayoutManager);
 
         Query query = FirebaseFirestore.getInstance().collection("peliculas").limit(50);
 
