4d4ec98ab4ede7b4518e4a1333ea18154e4454cb
Pol <pbermejo@elpuig.xeill.net>
2022-05-25 21:41:14 +0200

    ver info usuario y editarla

--- app/src/main/java/com/example/cinet/PerfilFragment.java
+++ app/src/main/java/com/example/cinet/PerfilFragment.java
@@ -6,14 +6,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -31,11 +36,11 @@ import java.util.Objects;
 
 public class PerfilFragment extends Fragment {
 
-    ImageView photoImageView;
-    TextView displayNameTextView, emailTextView, movilTextView, fechnacTextView;
-
+    ImageView photoImageView, lapiz_nombre, lapiz_email, lapiz_movil, lapiz_fecha_nacimiento;
+    EditText nombreEditText, emailEditText, movilEditText, fecha_nacimientoEditText;
     DatabaseReference reference;
     FirebaseDatabase database;
+    ExtendedFloatingActionButton extendedFloatingActionButton;
 
     public PerfilFragment() {}
 
@@ -52,10 +57,25 @@ public class PerfilFragment extends Fragment {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         photoImageView = view.findViewById(R.id.photoImageView);
-        displayNameTextView = view.findViewById(R.id.displayNameTextView);
-        emailTextView = view.findViewById(R.id.emailTextView);
-        movilTextView = view.findViewById(R.id.movilTextView);
-        fechnacTextView = view.findViewById(R.id.fechnacTextView);
+        nombreEditText = view.findViewById(R.id.nombreEditText);
+        emailEditText = view.findViewById(R.id.emailEditText);
+        movilEditText = view.findViewById(R.id.movilEditText);
+        fecha_nacimientoEditText = view.findViewById(R.id.fecha_nacimientoEditText);
+
+        extendedFloatingActionButton = view.findViewById(R.id.guardar_información);
+
+        lapiz_nombre = view.findViewById(R.id.lapiz_nombre);
+        lapiz_email = view.findViewById(R.id.lapiz_email);
+        lapiz_movil = view.findViewById(R.id.lapiz_movil);
+        lapiz_fecha_nacimiento = view.findViewById(R.id.lapiz_fecha_nacimiento);
+
+/*
+        edittextprueba.setFocusable(true);
+        edittextprueba.setClickable(true);
+        edittextprueba.setFocusableInTouchMode(true);
+        edittextprueba.setLongClickable(true);
+        edittextprueba.setInputType(InputType.TYPE_CLASS_PHONE);
+ */
 
         database = FirebaseDatabase.getInstance("https://cinet-cc0f5-default-rtdb.europe-west1.firebasedatabase.app/");
         assert user != null;
@@ -69,28 +89,88 @@ public class PerfilFragment extends Fragment {
                 System.out.println(user_data);
 
                 assert user_data != null;
-                displayNameTextView.setText(user_data.getNombre().toString());
-                emailTextView.setText(user_data.getEmail().toString());
-                movilTextView.setText(user_data.getMovil().toString());
-                fechnacTextView.setText(user_data.getFecha_de_nacimiento().toString());
+                nombreEditText.setText(user_data.getNombre().toString());
+                emailEditText.setText(user_data.getEmail().toString());
+                movilEditText.setText(user_data.getMovil().toString());
+                fecha_nacimientoEditText.setText(user_data.getFecha_de_nacimiento().toString());
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) { System.out.println("NO"); }
         });
 
+        lapiz_nombre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreEditText.setFocusable(true);
+                nombreEditText.setClickable(true);
+                nombreEditText.setFocusableInTouchMode(true);
+                nombreEditText.setLongClickable(true);
+                nombreEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+            }
+        });
 
+        lapiz_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emailEditText.setFocusable(true);
+                emailEditText.setClickable(true);
+                emailEditText.setFocusableInTouchMode(true);
+                emailEditText.setLongClickable(true);
+                emailEditText.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+            }
+        });
+
+        lapiz_movil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                movilEditText.setFocusable(true);
+                movilEditText.setClickable(true);
+                movilEditText.setFocusableInTouchMode(true);
+                movilEditText.setLongClickable(true);
+                movilEditText.setInputType(InputType.TYPE_CLASS_PHONE);
+            }
+        });
+
+        lapiz_fecha_nacimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fecha_nacimientoEditText.setFocusable(true);
+                fecha_nacimientoEditText.setClickable(true);
+                fecha_nacimientoEditText.setFocusableInTouchMode(true);
+                fecha_nacimientoEditText.setLongClickable(true);
+                fecha_nacimientoEditText.setInputType(InputType.TYPE_CLASS_DATETIME);
+            }
+        });
+
+        extendedFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database = FirebaseDatabase.getInstance("https://cinet-cc0f5-default-rtdb.europe-west1.firebasedatabase.app/");
+                reference = database.getReference("usuarios/"+user.getUid());
+
+                Query query = reference;//.orderByChild("entrada");
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            reference.child("nombre").setValue(nombreEditText.getText().toString());
+                            reference.child("email").setValue(emailEditText.getText().toString());
+                            reference.child("movil").setValue(movilEditText.getText().toString());
+                            reference.child("fecha_de_nacimiento").setValue(fecha_nacimientoEditText.getText().toString());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) { System.out.println("NO"); }
+                });
+            }
+        });
 
         if(user != null){
             if (FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl() != null) {
-                //displayNameTextView.setText(user.getDisplayName());
-                //emailTextView.setText(user.getEmail());
-
                 Glide.with(requireView()).load(user.getPhotoUrl()).into(photoImageView);
             } else {
-                //displayNameTextView.setText(user.getDisplayName());
-                //emailTextView.setText(user.getEmail());
-
                 Glide.with(requireView()).load(R.drawable.perfil).into(photoImageView);
             }
         }
